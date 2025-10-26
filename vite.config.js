a2ffdbabc6b84ad8e7a965a4e5d7b97c41e75aab
relja-dimitrijevic <dimitrijevicrelja2006@gmail.com>
2025-10-26 18:59:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/mmf-avantura-menu/'  // for GitHub Pages
-    : '/',                   // for localhost and Vercel
+  base: process.env.VITE_BASE_PATH || "/mmf-avantura-menu",
 })
